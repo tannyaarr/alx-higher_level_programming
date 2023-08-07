31a80cfc39ff1ab08efec2954be5084a2611c243
tannyaarr <tanyachiras@gmail.com>
2023-08-07 15:44:35 +0200

    10-check_cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * check if list is empty
+ * create pointer to head
+ * traverse through linked list
+ * if NULL is found return 0
+ * if pointer to head == head pointer
+ * return 1
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *temp = list;
