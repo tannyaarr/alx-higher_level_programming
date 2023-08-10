0ecb684d08922156244779e15a126c8d620b52dc
tannyaarr <tanyachiras@gmail.com>
2023-08-10 21:29:30 +0200

    0-add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    results = add(a, b)
+    print('{} + {} = {}'.format(a, b, results), end=' ')
+
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+add_results = add(a, b)
+sub_results = sub(a, b)
+mul_results = mul(a, b)
+div_results = div(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, add_results), end="")
+print("{:d} - {:d} = {:d}".format(a, b, sub_results), end="")
+print("{:d} * {:d} = {:d}".format(a, b, mul_results), end="")
+print("{:d} / {:d} = {:d}".format(a, b,div_results), end="")
