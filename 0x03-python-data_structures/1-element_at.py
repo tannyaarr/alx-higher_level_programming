4befd821cec365ecd92588f764df67d91d06e24a
tannyaarr <tanyachiras@gmail.com>
2023-08-12 22:28:42 +0200

    print element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
 def element_at(my_list, idx):
-    for idx in my_list
-
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
