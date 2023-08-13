6c1c150880abf74d5e3c4fb7f6d667bf9489b19d
tannyaarr <tanyachiras@gmail.com>
2023-08-13 02:50:55 +0200

    replace element in a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,11 +5,3 @@ def replace_in_list(my_list, idx, element):
     my_list[idx] = element
     return my_list
 
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
