2e3e9b15572b3cdc6f42a667c215cbf86591cf05
tannyaarr <tanyachiras@gmail.com>
2023-08-13 18:58:51 +0200

    print new list with original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_my_list = my_list[:]
-    if idx < 0 or idx >= len(new_my_list):
-        return(new_my_list)
-    new_my_list[idx] = element
-    return(new_my_list)
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(new_list):
+        return(new_list)
+    new_list[idx] = element
+    return(new_list)
