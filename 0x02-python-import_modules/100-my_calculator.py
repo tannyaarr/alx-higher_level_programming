19a5a2dc87d21fa0ce64e9e5635fb8bee9885fad
tannyaarr <tanyachiras@gmail.com>
2023-08-13 22:42:17 +0200

    fixed erros and indentation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-
-        operators = {"+": add, "-": sub, "*": mul, "/": div}
-        if operator not in operators:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-            result = operators[operator](a,b)
-            print(f"{a} {operator} {b} = {result}")
+    from sys import argv
+    argc = len(argv))
+    if len(argc) != 4:
+        print('Usage: {}  <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    ops = {
+        '+': add, 
+        '-': sub, 
+        '*': mul, 
+        '/': div
+    }
+    if argv[2] in ops:
+        a = int(argv[1])
+        op = ops[argv[2]]
+        b = int(argv[3])
+        result = op(a, b)
+        print('{:d} {:s} {:d} = {:d}'.format(a, argv[2], b, result))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    exit(0)
+        
