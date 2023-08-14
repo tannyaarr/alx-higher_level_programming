c0f785eed3d655f132e89b0ff76d7b29e36aa1ef
tannyaarr <tanyachiras@gmail.com>
2023-08-14 04:20:54 -0700

    removing all lower and uppercase c && git push c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,4 @@ def no_c(my_string):
     for char in my_string:
         if char != 'c' and char != 'C':
             new_string += char
-            return(new_string)
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+    return new_string
