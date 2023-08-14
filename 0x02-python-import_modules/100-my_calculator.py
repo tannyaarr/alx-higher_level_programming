321dae242ad2df9049327e3eb658207315ddea1c
tannyaarr <tanyachiras@gmail.com>
2023-08-14 03:11:59 -0700

    handled edge cases and fixed error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     argc = len(argv)
-    if len(argc) != 4:
+    if argc != 4:
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
     ops = {
