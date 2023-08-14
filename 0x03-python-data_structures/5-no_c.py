02aa311f6ae1b171744e8b15c64c7debe5e6f01d
tannyaarr <tanyachiras@gmail.com>
2023-08-14 13:31:37 +0200

    print a matrix of integers

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    for char in range(97, 12)
     new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
+        if char != "c" and char != "C":
             new_string += char
-            return(new_string)
+        return(new_string)
 
 print(no_c("Best School"))
 print(no_c("Chicago"))
