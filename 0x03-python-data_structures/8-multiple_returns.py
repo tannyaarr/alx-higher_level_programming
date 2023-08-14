d12ba1bbc23f91e3a45deeaf2d37f7a3347eb52f
tannyaarr <tanyachiras@gmail.com>
2023-08-14 14:34:38 +0200

    return first character and length of a sentence

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        first_char = None
+    else:
+        first_char = sentence[0]
+        length = len(sentence)
+        return length, first_char
