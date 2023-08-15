f59cf6a6e0e2b00df0052ddc76bdd3b6d05c9476
tannyaarr <tanyachiras@gmail.com>
2023-08-15 11:31:05 +0200

    delete at a particular index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if not my_list or idx < 0 or idx >= len(my_list):
         return my_list
-    return [value for i, value in enumerate(my_list) if i != idx ]
+    return [value for i, value in enumerate(my_list) if i != idx]
 
