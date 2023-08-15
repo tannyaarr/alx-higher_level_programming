bcaa1a1be3f4771f34865a3dca2b71b15e93cc0f
tannyaarr <tanyachiras@gmail.com>
2023-08-15 21:20:18 +0200

    linked test and palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,74 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *new_node, *tmp = *head;
-    listint_t *tmp2, *rev_head = malloc(sizeof(listint_t));
+    listint_t *slow = *head, *fast = *head, *prev_slow = NULL, *mid = NULL;
+    int is_palindrome = 1;
 
-    if (!rev_head)
-        return (-1);
- 
-    rev_head->n = tmp->n;
-    rev_head->next = NULL;
-    tmp = tmp->next;
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-    while (tmp != NULL)
+    while (fast && fast->next)
     {
-        new_node = malloc(sizeof(listint_t));
-        new_node->n = tmp->n;
-        new_node->next = rev_head;
-        rev_head = new_node;
-        tmp = tmp->next;
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
     }
 
-    tmp = *head;
-    tmp2 = rev_head;
-
-    while(tmp != NULL)
+    if (fast)
     {
-        if (tmp->n != tmp2->n)
-            return (0);
-        tmp = tmp->next;
-        tmp2 = tmp2->next;
+        mid = slow;
+        slow = slow->next;
     }
-    return (1);
+
+    listint_t *second_half = slow;
+    prev_slow->next = NULL;
+
+    listint_t *prev = NULL, *current = second_half, *next;
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    second_half = prev;
+
+    listint_t *first_half = *head;
+    while (first_half && second_half)
+    {
+        if (first_half->n != second_half->n)
+        {
+            is_palindrome = 0;
+            break;
+        }
+        first_half = first_half->next;
+        second_half = second_half->next;
+    }
+
+    prev = NULL, current = prev_slow->next;
+    prev_slow->next = NULL;
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    prev_slow->next = prev;
+
+    if (mid)
+    {
+        prev_slow->next = mid;
+        mid->next = second_half;
+    }
+    else
+    {
+        prev_slow->next = second_half;
+    }
+
+    return is_palindrome;
 }
