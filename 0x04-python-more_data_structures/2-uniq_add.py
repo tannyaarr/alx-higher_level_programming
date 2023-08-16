f9c55e8f6f3f06e388d0d9214cddd2c3f9198b3e
tannyaarr <tanyachiras@gmail.com>
2023-08-16 20:44:07 +0200

    unique add of integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    total_sum = 0
+    for num in my_list:
+        if num not in unique_integers:
+            total_sum += num
+            unique_integers.add(num)
+    return total_sum
