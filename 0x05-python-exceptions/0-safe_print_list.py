3cb30a038f8aa3c83fbde4160bd41285d773acf1
tannyaarr <tanyachiras@gmail.com>
2023-08-28 23:08:45 +0200

    prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,8 @@ def safe_print_list(my_list=[], x=0):
             if nb_print < x:
                 print(i, end="")
                 nb_print += 1
+    except TypeError:
+        pass
     except:
         pass
     print()
