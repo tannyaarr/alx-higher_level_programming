8d4e901414f5cf75a0f1b3b0f0a8acebcffc0c6a
tannyaarr <tanyachiras@gmail.com>
2023-08-29 22:24:27 +0200

    class that defines a square by size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Deine a class named Square"""
 
+
 class Square:
-     """Initialize a private attribute with an underscore prefix"""
     def __init__(self, size):
-        """private attribute indicating size of the square"""
         self._size = size
