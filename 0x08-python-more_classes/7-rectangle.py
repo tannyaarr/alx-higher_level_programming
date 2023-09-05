a1f54cd590bfbbd7afd3a9d3c21d97f0e81fec53
tannyaarr <tanyachiras@gmail.com>
2023-09-05 16:58:59 +0200

    class that defines a rectangle by 6-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define the class Rectangle based on -rectangle.py"""
 
+
 class Rectangle:
     """This represents the class Rectangle"""
 
@@ -12,7 +13,7 @@ class Rectangle:
         self._height = 0
         self.width = width
         self.height = height
-        Rectangle.number_of_instances+= 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -39,6 +40,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self._height = value
+
     def area(self):
         return self._width * self._height
 
@@ -59,4 +61,3 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
