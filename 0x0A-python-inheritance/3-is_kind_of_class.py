23c0c700d1acb775f14300c299ec350ce1fac3dc
tannyaarr <tanyachiras@gmail.com>
2023-09-12 22:45:40 +0200

    returns true if the object is inhertied directly or indirectly from the class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,7 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """returns true if the bject is an instance of a class that
+        inherited directly or indirectly from the specified class"""
+
     return isinstance(obj, a_class)
