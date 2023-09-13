92936ab89a7668a972abd4df9603552859b4e4b8
tannyaarr <tanyachiras@gmail.com>
2023-09-14 01:24:46 +0200

    search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -17,9 +17,3 @@ def append_after(filename="", search_string="", new_string=""):
             f.write(line)
             if search_string in line:
                 f.write(new_string + '\n')
-
-#test
-#!/usr/bin/python3
-append_after = __import__('100-append_after').append_after
-
-append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
