d8e494739eaec883554fff27d45d53bc43c4fad7
tannyaarr <tanyachiras@gmail.com>
2023-09-14 16:47:32 +0200

    stats

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,10 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
     if not filename:
         return
 
-    with open(filename, 'r', encoding='utf-8') as r:
+    with open(filename, 'r', encoding='utf-8') as f:
             lines = f.realines()
 
-    with open(filename, 'w', encoding='utf-8') as w:
+    with open(filename, 'w', encoding='utf-8') as f:
         for line in lines:
             f.write(line)
             if search_string in line:
