3d6a6755112c47e28aee30816f5ca5a450dfa93e
tannyaarr <tanyachiras@gmail.com>
2023-11-06 18:05:16 +0200

    prints lists but in sorted ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the class MyList"""
+
+
+class MyList(list):
+    """This represents MyList class"""
+
+    def __init__(self):
+        """Instatiation of MyList class"""
+        list.__init__(self)
+
+    def print_sorted(self):
+        print(sorted(self))
