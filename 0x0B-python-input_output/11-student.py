c8c8ce40efca226bd090685fa5f5138c74312fae
tannyaarr <tanyachiras@gmail.com>
2023-11-07 18:45:49 +0200

    student to json with filler

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
+"""Defines the students class using JSON"""
+
 
 class Student:
+    """This represents the class Student"""
+
     def __init__(self, first_name, last_name, age):
-        """Initialization method for Student class"""
+        """Initialize first and last name and age"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance"""
+        """define to_json and attrs"""
+
         if attrs is None:
             return self.__dict__
-        return {key: value for key, value in self.__dict__.items() if key in attrs}
+        else:
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance"""
+        """this replaces all attributes of the student instance"""
+
         for key, value in json.items():
             setattr(self, key, value)
-
