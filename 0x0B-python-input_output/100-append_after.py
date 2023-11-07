dc3e4028c957e5dd712b32120ad59bf9995d3385
tannyaarr <tanyachiras@gmail.com>
2023-11-07 18:38:49 +0200

    inserts a line of text to a file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the append search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line a text to a file after each line containing
+        a specific string"""
+
+    if not filename:
+        return
+
+    with open(filename, 'r', encoding='utf-8') as f:
+            lines = f.realines()
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string + '\n')
+
