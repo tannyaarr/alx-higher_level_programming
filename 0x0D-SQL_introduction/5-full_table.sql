195faab4c86a0c75d96ec6add87b0e4dbd5201f6
tannyaarr <tanyachiras@gmail.com>
2023-12-12 21:04:04 +0200

    prints the full description of the table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Prints the full description of the table first_table from the database hbtn_0c_0
 
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = hbtn_0c_0 and first_table = first_table
+SHOW CREATE TABLE first_table;
