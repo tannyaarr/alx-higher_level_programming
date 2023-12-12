fa6d44354ec6767cd1772a5c7adf466f1c8a56f5
tannyaarr <tanyachiras@gmail.com>
2023-12-12 20:41:55 +0200

    creates a table called first_table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Script that creates a table called first_table in the current database
 
 
-CREATE TABLE IF NOT EXISTS first_table (id NT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
