0cbb9b1cba8f3479cb3182293a34dd1b09c54586
tannyaarr <tanyachiras@gmail.com>
2023-12-13 23:12:56 +0200

    lists all the tv shows contained in the database

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genre 
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
