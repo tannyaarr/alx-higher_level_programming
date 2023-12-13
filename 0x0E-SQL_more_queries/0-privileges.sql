7d3579cf06cc8001936a9ad9b360ec0488dfc236
tannyaarr <tanyachiras@gmail.com>
2023-12-13 21:24:19 +0200

    lists all privileges of mysql users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- lists all privileges of MySQl users user_0d_1 and user_0d_2
 
 
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost'
+SHOW GRANT FOR 'user_0d_2'@'localhost'
