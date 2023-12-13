4c155f044b93ef3149c826e2b2791e01edf36fc6
tannyaarr <tanyachiras@gmail.com>
2023-12-13 23:19:09 +0200

    lists all shows and all genres linked to that tv_show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows and all genres linked to that tv_show
 
-SELECT tv_shows.titles, tv_genres.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
