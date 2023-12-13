38495117c3a377fb901ac512e0a52805df80cff7
tannyaarr <tanyachiras@gmail.com>
2023-12-13 23:41:12 +0200

    lists all genres and display the number of shows linked

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all Comedy shows
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS num ber_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS num ber_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
