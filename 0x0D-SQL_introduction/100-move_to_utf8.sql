8a54630722a7fd860f76495e3800e6584e2ca6e5
tannyaarr <tanyachiras@gmail.com>
2023-12-14 22:11:45 +0200

    converts hbtn_0d_0 database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@
 
 ALTER DATABASE hbtn_0d_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0d_0;
-ALTER TABLE first_name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
