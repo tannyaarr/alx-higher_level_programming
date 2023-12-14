07688324f79ea16771191b704350a6a66bc102e0
tannyaarr <tanyachiras@gmail.com>
2023-12-14 22:24:14 +0200

    displays the average temperature fahrenheit by city

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,8 @@
 -- displays the average temperature
 
 
-SELECT city, AVG((temperature * 9/5) + 32) AS avg_tempeRATURE_fahrenheit
+SELECT city, AVG(value) 
+AS avg_temperature_fahrenheit
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temperature_fahrenheit DESC;
