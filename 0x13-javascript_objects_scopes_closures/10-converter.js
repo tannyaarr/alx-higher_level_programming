fad6989bde0b658cff8fa46ca0a331668e172e4d
tannyaarr <tanyachiras@gmail.com>
2024-01-09 23:21:20 +0200

    converts a number base 10 to another base passed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function convertRecursive (number) {
-    if (number === 0) {
-      return '';
-    }
-    const remainder = number % base;
-    const quotient = Math.floor(number / base);
-    return convertRecursive(quotient) + remainder;
-  }
   return function (number) {
-    return number === 0 ? '0' : convertRecursive(number);
+    return number.toString(base);
   };
 };
