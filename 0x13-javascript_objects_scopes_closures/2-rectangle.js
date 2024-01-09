b7cf703e6655fc55dfa6d39aa76e1d3c6ca9aed1
tannyaarr <tanyachiras@gmail.com>
2024-01-09 20:06:07 +0200

    class rectangle the constructor that takes 2 arguments

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
 	constructor(w, h) {
-		if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+		if (w > 0) && (h > 0) {
 			return {};
 		}
-
 		this.width = w;
 		this.height = h;
 	}
