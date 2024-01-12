cfef74467b65a93134b0a3454b7af4dfa06faf11
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 11:55:05 -0800

    class definition of a state and an instnace

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""""File contains the class definition of a state"""
 
 from sqlalchemy import create_engine, Column, Integer, String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +8,8 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class State(Base):
+    """Class definition of State"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True,
-            name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
