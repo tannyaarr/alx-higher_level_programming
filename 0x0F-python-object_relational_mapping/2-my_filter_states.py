cb5298dda417829510da4ef231c5e1d24cf6be64
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 06:52:22 -0800

    scripts that takes in a arguemnt and displays all values in the states table

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Scripts that displays all values in the states table of hbtn_0e_0_usa where name matches that provided arguemnt"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    usr, pwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    ht, pt = "localhost", 3306
+
+    conn = MySQLdb.connect(host=ht, port=pt, user=usr, passwd=pwd, db=db)
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id")
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
