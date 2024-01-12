197ae2b7bbc8ff8f4f3b5cafb408a3aec71b2f75
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 07:37:55 -0800

    scripts that takes in a arguemnt and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,15 +10,13 @@ if __name__ == "__main__":
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    usr, pwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3],
-    sys.argv[4]
+    usr, pwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     ht, pt = "localhost", 3306
 
     conn = MySQLdb.connect(host=ht, port=pt, user=usr, passwd=pwd, db=db)
 
     cursor = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id"\
-            .format(state_name)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id".format(state_name)
     cursor.execute(query)
     rows = cursor.fetchall()
 
