ce0b985c3ce88865f3b8adae9ca4ca5ab2d1bb19
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 12:54:24 -0800

    lists the state starting with a objects from the database

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).filter(State.name.ilike('%a%'))
+    .order_by(State.id).all()
 
     for state in states_with_a:
         print(f"{state.id}: {state.name}")
