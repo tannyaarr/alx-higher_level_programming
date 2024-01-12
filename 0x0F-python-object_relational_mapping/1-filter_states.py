f58ceaa24510fbddea9f5092b3cf0ebe4460def3
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 06:36:20 -0800

    scripts that lists all sttes with s name strting with the letter N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Script that lists all states with the name starting with N from the databse hbtn_0e_0_usa"""
+"""Script that lists all states with the name starting with N from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
