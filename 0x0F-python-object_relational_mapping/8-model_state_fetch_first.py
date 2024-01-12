1ae1b0b890f4168f99b5bbc877d957944299fb48
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 12:45:05 -0800

    lists the first  state objects from the database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Scripts prints the first State objects from database"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    usr = args[1]
+    pwd = args[2]
+    db = args[3]
+    ht = "localhost"
+    pt = 3306
+
+    db_string = f"mysql://{usr}:{pwd}@{ht}:{pt}/{db}"
+    engine = create_engine(db_string)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(state.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state_name}")
+    else:
+        print("Nothing")
+
+    session.close()
+~                     
