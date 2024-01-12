1005207ecf46bc3c9a4733f2f3fe57dfd56d98a3
tannyyaarr <tanyachiras@gmail.com>
2024-01-12 09:46:41 -0800

    takes in arguments and displays all value safe from mysql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,23 +7,19 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database>
-                <state_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    usr, pwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-    state_name = sys.argv[4]
-    ht, pt = "localhost", 3306
+    args = sys.argv
+    usr = args[1]
+    pwd = args[2]
+    db = args[3]
+    state_name = args[4]
+    ht  = "localhost"
+    pt = 3306
 
     conn = MySQLdb.connect(host=ht, port=pt, user=usr, passwd=pwd, db=db)
-
     cursor = conn.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
 
