bee509ad85f3edd4ab4c24249c37f329e07f3220
tannyyaarr <tanyachiras@gmail.com>
2024-01-25 06:50:03 -0800

    sends a GET request to the url

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request to the URL
-response=$(curl -s -o /dev/null -w "%{http_code}" $1)
-if [ $response -eq 200 ]
-then
-    curl -s $1
-else
-    echo "Error: Response code $response"
-fi
+curl -s -X GET $1
