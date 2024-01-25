a98c529eaf8472aefe48fcc75d5485bb84f031bf
tannyyaarr <tanyachiras@gmail.com>
2024-01-25 06:32:42 -0800

    displays the size of the body of the reponse

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script that takes in a url amd sends a request and displays the size of the body reponse
+# Script that takes in a url and sends a request and displays the size of the body reponse
 
-url="$1"
-curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r'
+response=$(curl -s -o /dev/null -w "%{size_download}" $1)
+echo "The size of the reponse body is $reponse bytes."
