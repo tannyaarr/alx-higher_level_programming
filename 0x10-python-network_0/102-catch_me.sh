515ae0618cc7eb246d9cb2cd7ad6ab421f2319dd
tannyyaarr <tanyachiras@gmail.com>
2024-01-25 13:51:41 -0800

    script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to make a request that causes the server to respond with "You got me!"
-curl -X PUT -L --data "user_id=98" --header "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
