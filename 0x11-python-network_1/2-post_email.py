114e39cd32ae9878b9586d566aaefde182ba2e17
tannyyaarr <tanyachiras@gmail.com>
2024-01-27 02:29:15 -0800

    displays email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,12 +5,13 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    the_page = response.read().decode('utf-8')
-    print(the_page)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode('utf-8')
+        print(the_page)
