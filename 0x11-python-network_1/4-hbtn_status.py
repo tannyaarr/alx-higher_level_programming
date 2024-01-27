c28348ad3fdd874061d0fc7654287f29a6c90924
tannyyaarr <tanyachiras@gmail.com>
2024-01-27 05:18:45 -0800

    displays the value of the variable X-Request-Id

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     response = requests.get(url)
     print("- Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- content: {}".format(response.content))
