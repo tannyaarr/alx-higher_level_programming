35b52d8e0e4db5bad3eafff21bcd7f186e7d7e73
tannyyaarr <tanyachiras@gmail.com>
2024-01-27 05:37:22 -0800

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Takes in a URl and a email address"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    print(response.text)
