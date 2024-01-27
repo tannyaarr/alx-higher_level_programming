4d0306dfcd3b12031f997df6a3ee32aeface9e28
tannyyaarr <tanyachiras@gmail.com>
2024-01-27 05:07:48 -0800

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,6 @@ import requests
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
-    response.encoding = 'utf-8'
     print("- Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
-    print("\t- utf8 content: {}".format(response.text))
