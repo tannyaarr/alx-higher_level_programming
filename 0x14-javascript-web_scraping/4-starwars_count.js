8502edfa845f3c62810b67fb9f73cecae21f189f
tannyaarr <tanyachiras@gmail.com>
2024-02-21 17:03:45 +0200

    semistandard fixed + rewritten file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,14 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 if (!apiUrl) {
-    console.log("Usage: ./count_wedge_movies.js <api_url>");
+  console.log('Usage: ./count_wedge_movies.js <api_url>');
 } else {
-    const characterId = 18;
+  const characterId = 18;
 
-    request.get(apiUrl, { json: true }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const wedgeMovies = body.results.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-            console.log(wedgeMovies.length);
-        }
-    });
+  request.get(apiUrl, { json: true }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const wedgeMovies = body.results.filter(movie => movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+      console.log(wedgeMovies.length);
+    }
+  });
 }
