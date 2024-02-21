cec427b267ad01e58afa89e9c8e509a653f7a1d4
tannyaarr <tanyachiras@gmail.com>
2024-02-21 17:14:42 +0200

    semistandard fixed + rewritten file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
+
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, (error, response, body) => {
-	if (!error && response.statusCode === 200) {
-		const tasksData = JSON.parse(body);
+  if (!error && response.statusCode === 200) {
+    const tasksData = JSON.parse(body);
 
-		const completedTasks = tasksData.filter((task) => task.completed);
-		const tcompletedTasksByUser = completedTasks.reduce((countByUser, task) => {
-		countByUser[task.userId] = (countByUser[task/userId] || 0) +1;
-		return countByUser;
-		}, {});
-		console.log(completedTasksByUser);
-	} else {
-		console.error('Error: ${error}');
-	}
+    const completedTasks = tasksData.filter((task) => task.completed);
+
+    const completedTasksByUser = completedTasks.reduce((countByUser, task) => {
+      countByUser[task.userId] = (countByUser[task.userId] || 0) + 1;
+      return countByUser;
+    }, {});
+
+    console.log(completedTasksByUser);
+  } else {
+    console.error(`Error: ${error}`);
+  }
 });
