f62dd89d977a867f3a03da4781cd0a56c47bfbe7
tannyaarr <tanyachiras@gmail.com>
2024-02-21 17:11:44 +0200

    semistandard fixed + rewritten file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,15 +4,4 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 const fs = require('fs');
 
-if (!url || !filePath) {
-  console.log('Usage: ./get_webpage.js <url> <file_path>');
-} else {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(`Error requesting the URL: ${error.message}`);
-    } else {
-      fs.writeFileSync(filePath, body, 'utf-8');
-      console.log(`Webpage content successfully written to ${filePath}`);
-    }
-  });
-}
+request(url).pipe(fs.createWriteStream(filePath));
